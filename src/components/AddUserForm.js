274e4b4d16af287437a042c6cc9121d0ab8a11a2
Deborah Animashaun <deborahanimashaun659@yahoo.com>
2022-10-17 13:57:33 +0100

    Feat: Refactor add user form - Add unique id for users - lift user data and state up to app.js

--- src/components/AddUserForm.js
+++ src/components/AddUserForm.js
@@ -1,28 +1,53 @@
-import { useState } from "react"
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function AddUserForm(){
-    const [nameValue, setNameValue]=useState("")
-    const [ageValue, setAgeValue]=useState("")
+function AddUserForm(props) {
+  const [userName, setUserName] = useState("");
+  const [userAge, setUserAge] = useState("");
 
-    function nameChangeHandler(e){
-        console.log(e.target.value)
-        setNameValue(e.target.value)
-    }
-    
-    function ageChangeHandler(e){
-        console.log(e.target.value)
-        setAgeValue(e.target.value)
-    }
-    return <div>
-        <form>
-            <div>
-            <input value={nameValue} onChange={nameChangeHandler} placeholder="name" type="text" />
-            <input  value={ageValue} onChange={ageChangeHandler}placeholder="age" type="text" />
-            </div>
+  function nameChangeHandler(e) {
+    // console.log(e.target.value);
+    setUserName(e.target.value);
+  }
 
-            <button> Add User </button>
-        </form>
+  function ageChangeHandler(e) {
+    // console.log(e.target.value);
+    setUserAge(e.target.value);
+  }
+
+  function submitHandler(e) {
+    e.preventDefault();
+    let userData = {
+      name: userName,
+      age: userAge,
+      id: uuidv4(),
+    };
+    props.onAddUser(userData);
+    console.log(userData);
+    console.log(`in add user form` + userData);
+  }
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <div>
+          <input
+            value={userName}
+            onChange={nameChangeHandler}
+            placeholder="name"
+            type="text"
+          />
+          <input
+            value={userAge}
+            onChange={ageChangeHandler}
+            placeholder="age"
+            type="text"
+          />
+        </div>
+
+        <button> Add User </button>
+      </form>
     </div>
+  );
 }
 
-export default AddUserForm
+export default AddUserForm;
